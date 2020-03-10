11b4819167fb1bfdd6ee2dd0f7a9b7c4a179d227
Abhinash <abhinash.pandey@travelkosh.com>
2020-03-10 17:59:00 +0530

    initial setuu

---
+++ src/test/java/com/abhinash/shoppingdashboard/ShoppingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinash.shoppingdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
